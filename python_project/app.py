90a27b728bfd6e8c1014b9cda1286f9fa51aec70
DAVI ANTONINO NUNES DA SILVA <118570287+dansfisica85@users.noreply.github.com>
2026-09-18 23:55:32 +0000

    Implement basic Flask application with home route and template rendering

---
+++ python_project/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    #dados que serãoi calculados pelo Python
+    titulo_pagina = "Dashboard de Demonstração"
+    aluno = {"nome": "Lucas", "curso": "Analista de Sistemas", "nota": 9.5}
+    status = "Aprovado" if aluno["nota"] >= 5 else "Reprovado"
+
+    #Essa parte envia as imnformações para o template HTML
+    return render_template(
+    "index.html",
+    titulo=titulo_pagina,
+    usuario=aluno,
+    resultado=status
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+     

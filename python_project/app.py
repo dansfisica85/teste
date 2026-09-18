8071a1d4e83686ec57ed8c7ad4ec04224f1799c5
DAVI ANTONINO NUNES DA SILVA <118570287+dansfisica85@users.noreply.github.com>
2026-09-18 23:59:10 +0000

    Fix typo in comment within home route function

--- python_project/app.py
+++ python_project/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    #dados que serãoi calculados pelo Python
+    #dados que serão calculados em Python
     titulo_pagina = "Dashboard de Demonstração"
     aluno = {"nome": "Lucas", "curso": "Analista de Sistemas", "nota": 9.5}
     status = "Aprovado" if aluno["nota"] >= 5 else "Reprovado"
